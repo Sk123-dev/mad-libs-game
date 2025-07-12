f366d9bfebcded37cee5aff1ff21b122371d9674
sk123-dev <safan.faisal@outlook.com>
2025-07-12 17:18:32 +0500

    Update PROJECT1.py

--- PROJECT1.py
+++ PROJECT1.py
@@ -1,9 +1,12 @@
 #Madlbis game, word game where you create a story by filling in blanks with random words#
-adjective1 = input("Entire an adjective (description): ")
+
+#Ask the user to input a series of adjectives,nouns,verbs, since this is basic it is only limited to this#
+adjective1 = input("Entire an adjective (description): ") 
 noun1 = input("Enter a noun(person, place, thing): ")
 adjective2 = input("Entire an adjective (description): ")
 verb1 = input("Enter a verb ending with 'ing': ")
 adjective3 = input("Entire an adjective (description): ")
+
 
 print(f"Today I went to a {adjective1} zoo.")
 print(f"In an exhibit, I saw a {noun1}")
